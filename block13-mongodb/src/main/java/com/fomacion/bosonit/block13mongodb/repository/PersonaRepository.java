2690fd567561503dd91aa330d53cb2b5c2c08cd2
eduardoJimeno <eduardo.jimeno@bosonit.com>
2024-01-08 10:35:49 +0100

    Cambio MongoTemplate + Conexión Atlas

--- block13-mongodb/src/main/java/com/fomacion/bosonit/block13mongodb/repository/PersonaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fomacion.bosonit.block13mongodb.repository;
-
-import com.fomacion.bosonit.block13mongodb.entity.Persona;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PersonaRepository extends MongoRepository<Persona, String> {
-}
